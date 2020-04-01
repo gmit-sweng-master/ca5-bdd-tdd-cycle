d347d3da290101cae8cda117a0378d4103b91c13
John French <john.french@gmit.ie>
2020-04-01 15:04:34 +0000

    Added skeleton rspec tests

---
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe MoviesController, type: 'controller' do
+
+  describe "#director" do
+    context "When specified movie has a director" do
+      it "should find movies with the same director" do
+        ## YOUR TEST CODE HERE
+        skip("Not yet implemented")
+      end
+    end
+
+    context "When specified movie has no director" do
+      it "should redirect to the movies page" do
+        ## YOUR TEST CODE HERE
+        skip("Not yet implemented")
+      end
+    end
+  end
+end
---
+++ rottenpotatoes/spec/models/movie_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Movie do
+  describe "#similar" do
+    it "should find movies by the same director" do
+        ## YOUR TEST CODE HERE
+        skip("Not yet implemented")
+    end
+
+    it "should not find movies by different directors" do
+        ## YOUR TEST CODE HERE
+        skip("Not yet implemented")
+    end
+  end
+end
